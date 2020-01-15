6be73e2ea7fd3a9661b0f161b0716f075ce43574
evgeniy.kreerenko <evgeniykreerenko@dunice.net>
2020-01-15 14:33:33 +0300

    todo from string to onject type

--- src/scripts/Item/index.jsx
+++ src/scripts/Item/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Item = (props) => {
-
+const Item = ({item, index, deleteTodo}) => {
+  console.log('item ', item);
+  return (
+    <>
+      <div >{item.title}</div>
+      <button onClick={() => deleteTodo(`${item.title}${index}`)}>delete todo</button>
+    </>
+  )
 };
 
-export default React.memo(Item);
+export default React.memo(Item, (props, prevProps) => !Object.is(props, prevProps));
--- src/scripts/List/index.jsx
+++ src/scripts/List/index.jsx
@@ -1,23 +1,37 @@
-import React, { useState, useCallback, Fragment } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import styles from './index.css';
+import Item from "../Item";
+
+const defaultList = [
+  {
+    title: '123',
+    status: false,
+  },
+  {
+    title: '456',
+    status: false,
+  }
+];
 
 const TodoList = () => {
 
   const [title, setTodoTitle] = useState('');
-  const [todoList, setTodoList] = useState(['123', '456']);
+  const [todoList, setTodoList] = useState(defaultList);
 
   const handleChange = useCallback(event => setTodoTitle(event.target.value), []);
 
   const addTodo = useCallback(() => {
-    console.log('todoList', todoList);
-    setTodoList(todoList => [...todoList, title]);
-    setTodoTitle('');
-  }, [title]);
+    setTodoTitle(prevTitle => {
+      console.log('todoList', todoList);
+      setTodoList(todoList => [...todoList, { title: prevTitle, status: false }]);
+      return '';
+    });
+  }, []);
 
   const deleteTodo = useCallback(title => {
     console.log('id', title);
-    setTodoList(list => list.filter((item, index) => `${item}${index}` !== title))
+    setTodoList(list => list.filter((item, index) => `${item.title}${index}` !== title))
   }, []);
 
   console.log('todoList on Render', todoList);
@@ -27,10 +41,7 @@ const TodoList = () => {
       <input name="title" type="text" value={title} onChange={handleChange} />
       <button onClick={addTodo}>add todo</button>
       {todoList.map((item, index) => (
-        <Fragment key={`${item}${index}`}>
-          <div >{item}</div>
-          <button onClick={() => deleteTodo(`${item}${index}`)}>delete todo</button>
-        </Fragment>
+        <Item key={`${item.title}${index}`} item={item} index={index} deleteTodo={deleteTodo} />
       ))}
     </div>
   )
