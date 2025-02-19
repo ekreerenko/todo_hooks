5db8778268b04b659a5f44524e82c814fe3694b1
Evgeniy Kreerenko <evgeniy.kreerenko@gmail.com>
2025-02-19 12:43:40 +0300

    change entry point

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './scripts';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(<App />);
